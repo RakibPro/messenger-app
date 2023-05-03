e08c18450b17908cb283f71043b9b4058a5ff439
Rakib <rakibpro98@gmail.com>
2023-05-04 02:46:27 +0600

    home page serverSide render added and loading state added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import { Inter } from 'next/font/google';
 import ChatInput from './components/ChatInput';
 import MessageList from './components/MessageList';
+import { Message } from '@/typing';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
+    (res) => res.json()
+  );
+
+  const messages: Message[] = data.messages;
+
+  console.log(data);
   return (
     <main>
-      <MessageList />
+      <MessageList initialMessages={messages} />
       <ChatInput />
     </main>
   );
